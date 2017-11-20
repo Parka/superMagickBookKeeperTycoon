4be49ed8aa96961d738aab2972a16a141880a531
sbanchero <sbanchero@analiticar.com.ar>
2017-11-20 19:53:12 -0300

    Added test assests

---
+++ Assets/scripts/book/BookInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookInfo : MonoBehaviour {
+
+    public string bookName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
