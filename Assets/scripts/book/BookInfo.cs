6c80a07a7d5c9b4dd592f3913e358a2cbb350d93
Lucas Berdiales <lberdiales@gmail.com>
2017-11-22 21:22:13 -0300

    Added enums for BookType and BookTopic. Created BookManager for spawning books and randomize prefab instances.

--- Assets/scripts/book/BookInfo.cs
+++ Assets/scripts/book/BookInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class BookInfo : MonoBehaviour {
-
+	public BookManager.BookType type;
+	public BookManager.BookTopic topic;
     public string bookName;
 
 	// Use this for initialization
---
+++ Assets/scripts/book/BookManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookManager : MonoBehaviour {
+	public enum BookType {
+		Magic,
+		Story,
+		SelfHelp
+		// ...
+	}
+
+	public enum BookTopic {
+		Elements,
+		BlackMagic,
+		Vampires,
+		Dating
+		// ...
+	}
+
+	public Transform spawnPoint;
+	public float spawnTime = 5f;
+	public GameObject book;
+
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	void Spawn() {
+		Randomize (Instantiate (book, spawnPoint.position, spawnPoint.rotation));
+	}
+
+	public void Randomize(GameObject book) {
+		SpriteRenderer sr = book.GetComponent<SpriteRenderer> ();
+		BookInfo bi = book.GetComponent<BookInfo> ();
+
+		sr.color = new Color (Random.value, Random.value, Random.value, 1.0f);
+
+		bi.type = (BookManager.BookType)Random.Range(0, System.Enum.GetValues(typeof(BookManager.BookType)).Length);
+		bi.topic = (BookManager.BookTopic)Random.Range(0, System.Enum.GetValues(typeof(BookManager.BookTopic)).Length);
+		bi.bookName = bi.type.ToString () + "_" + bi.topic.ToString ();
+	}
+}
