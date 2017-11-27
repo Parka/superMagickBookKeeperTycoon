7bbd755d6d1ebca3e051a585611deb8ead50fcaf
sbanchero <sbanchero@analiticar.com.ar>
2017-11-26 21:00:16 -0300

    Added "Flame" logic

---
+++ Assets/scripts/book_effects/Flame.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour {
+	[SerializeField]
+	private Collider2D colliderPropagateNear;
+	[SerializeField]
+	private Collider2D colliderPropagateFar;
+
+	[SerializeField]
+	private int propagateFarChances = 10;
+	[SerializeField]
+	private int propagateNearChances = 5;
+
+	private double timeLeft = 0.5;
+
+	private HashSet<GameObject> targetBooksNear = new HashSet<GameObject>();
+	private HashSet<GameObject> targetBooksFar = new HashSet<GameObject>();
+	private GameObject affectedBook;
+
+	public double decaySpeed = 5;
+	public double decay = 100;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		timeLeft -= Time.deltaTime;
+		if (timeLeft <= 0) {
+			//"Randomly" propagate to nearby books
+			propagate (targetBooksNear, propagateNearChances);
+			propagate (targetBooksFar, propagateFarChances);
+			timeLeft = 0.5;
+		}
+
+		//Flame decay
+		decay -= Time.deltaTime * decaySpeed;
+		if (decay <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	private void propagate(HashSet<GameObject> targetBooks, int chances){
+		GameObject cloneFlame;
+		foreach (GameObject book in targetBooks) {
+			if (book == affectedBook)
+				continue;
+			if (book.GetComponentsInChildren<Flame> ().GetLength(0)==0) {
+				int dice = Random.Range ((int)0, chances);
+				if (dice == 0) {
+					cloneFlame = Instantiate (gameObject);
+					cloneFlame.transform.SetParent (book.transform);
+					cloneFlame.transform.position = book.transform.position;
+					if (cloneFlame.GetComponent<Flame> ().decay < 90)
+						cloneFlame.GetComponent<Flame> ().decay += 10;
+
+				}
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Book" && collider.IsTouching(colliderPropagateNear))
+		{
+			targetBooksNear.Add(collider.gameObject);
+		}
+		if (collider.gameObject.tag == "Book" && collider.IsTouching(colliderPropagateFar))
+		{
+			targetBooksFar.Add(collider.gameObject);
+		}
+	}
+	private void OnTriggerExit2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Book" && !collider.IsTouching(colliderPropagateNear))
+		{
+			targetBooksNear.Remove(collider.gameObject);
+		}
+		if (collider.gameObject.tag == "Book" && !collider.IsTouching(colliderPropagateFar))
+		{
+			targetBooksFar.Remove(collider.gameObject);
+		}
+	}
+
+	private void LateUpdate(){
+		transform.rotation = Quaternion.Euler (0, 0, 0);
+	}
+
+}
